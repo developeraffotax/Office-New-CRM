fd071ca921739db35df26a1915d756382fd73d69
developeraffotax <developer@affotax.com>
2024-09-12 18:46:06 +0500

    thursday2

--- client/public/env.js
+++ client/public/env.js
@@ -4,7 +4,7 @@ window.env = {
   "APPDATA": "C:\\Users\\HP\\AppData\\Roaming",
   "ChocolateyInstall": "C:\\ProgramData\\chocolatey",
   "ChocolateyLastPathUpdate": "133415793963545845",
-  "CHROME_CRASHPAD_PIPE_NAME": "\\\\.\\pipe\\crashpad_6692_EVNDUCISRJNKZDCV",
+  "CHROME_CRASHPAD_PIPE_NAME": "\\\\.\\pipe\\crashpad_14396_ALTSQUCMNTJKOPBF",
   "COLOR": "1",
   "COLORTERM": "truecolor",
   "CommonProgramFiles": "C:\\Program Files\\Common Files",

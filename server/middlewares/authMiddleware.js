5706bb5c631beacbc037567bb1d83ad366ae9001
developeraffotax <developer@affotax.com>
2024-10-02 13:10:46 +0500

    wednesday

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -41,7 +41,7 @@ export const isAdmin = async (req, res, next) => {
       });
     }
 
-    if (user.role !== "Admin") {
+    if (user.role.name !== "Admin") {
       return res.status(401).send({
         success: false,
         message: "Forbidden! User does not have admin privileges.!",

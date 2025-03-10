7f1a887ab669e34b470e9cb9994b5d3f4cb84f30
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-03-10 15:01:19 +0500

    Job_Holder Summery Filters | bug fixes

--- client/src/pages/Tasks/CompletedTasks.js
+++ client/src/pages/Tasks/CompletedTasks.js
@@ -25,7 +25,7 @@ import { format } from "date-fns";
 import { useLocation } from "react-router-dom";
 import { mkConfig, generateCsv, download } from "export-to-csv";
 import JobCommentModal from "../Jobs/JobCommentModal";
-import TaskDetail from "./TaskDetail";
+import TaskDetail from "./RowTaskDetail";
 import { MdBackspace } from "react-icons/md";
 
 // CSV Configuration

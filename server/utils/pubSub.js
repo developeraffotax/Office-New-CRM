91d99b69dab98c7fd6ddea0a35e5634a409f4b45
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-03-20 18:13:43 +0500

    task misc update

--- server/utils/pubSub.js
+++ server/utils/pubSub.js
@@ -1,6 +1,8 @@
 
  
 import { PubSub } from '@google-cloud/pubsub';
+import { JWT } from 'google-auth-library';
+import { google } from 'googleapis';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -12,18 +14,14 @@ const __dirname = path.dirname(__filename);
 
 
 // Path for Credentials
-const CREDENTIALS_PATH = path.join(
-    __dirname,
-    "..",
-    "config",
-    "service-pubsub.json"
-  );
+const CREDENTIALS_PATH = path.join( __dirname, "..", "config", "service-pubsub.json" );
 
 
 
 const pubSubClient = new PubSub({
     projectId: 'affotax-crm',
     keyFilename: CREDENTIALS_PATH
+    
     });
 
 
@@ -31,20 +29,94 @@ const subscriptionName = 'projects/affotax-crm/subscriptions/ticket-notification
  
 
  
+
+
+
+
+
+
+
+
+  // Scopes you need
+  const SCOPES = [ "https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/gmail.modify", "https://www.googleapis.com/auth/pubsub",  ];
+
+  // Create a JWT client using the Service Account key
+  const jwtClient = new JWT({
+    keyFile: CREDENTIALS_PATH,
+    scopes: SCOPES,
+    subject: "info@affotax.com",
+  });
+
+
+
+
  
 export function listenForMessages() {
   const subscription = pubSubClient.subscription(subscriptionName);
 
-  const messageHandler = (message) => {
-    console.log(`Received message: ${message.id}`);
-    console.log(`Data: ${message.data}`);
-    
-    // Here, process the notification message, e.g., fetch the email details via Gmail API
-    // You can store the email info in your database or trigger another action.
 
-    // Acknowledge the message
-    message.ack();
-  };
+  const messageHandler = async (message) => {
+    // console.log("THE RECEIVED MESSAGE", message);
+    // console.log(`Received message: ${message.id}`);
+
+    try {
+        const data = Buffer.from(message.data, 'base64').toString();
+        const { historyId } = JSON.parse(data);
+
+
+        await jwtClient.authorize();
+        const gmail = google.gmail({ version: 'v1', auth: jwtClient });
+
+        // 1. Get History
+        const historyResponse = await gmail.users.history.list({
+            userId: 'me',
+            startHistoryId: historyId,
+            
+            
+        });
+
+        const history = historyResponse.data.history;
+
+        
+        if (history && history.length > 0) {
+
+
+            for (const item of history) {
+
+               
+              console.log("ITEM", item.messages);
+                if (item.messages  && item.messages.length > 0) {
+                    for (const message of item.messages) {
+                        // const messageId = addedMessage.message.id;
+                        const threadId = message.threadId;
+                         
+                        //const recipient = message.payload.headers.find(header => header.name === 'To').value;
+
+
+                        console.log("THREAD ID", threadId);
+                         
+                        
+                    }
+                }
+            }
+        } else {
+            console.log('No new messages related to historyId:', historyId);
+        }
+    } catch (error) {
+        console.error('Error processing message:', error);
+    } finally {
+        message.ack();
+    }
+};
+
+
+
+
+
+
+
+
+
 
   subscription.on('message', messageHandler);
 }

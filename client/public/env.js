5bd5fdb3a38f000469427d3a89a2c3d4e40af66b
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-03-21 16:35:07 +0500

    pubSub push add

--- client/public/env.js
+++ client/public/env.js
@@ -54,7 +54,7 @@ window.env = {
   "POSH_CURSOR_LINE": "7",
   "POSH_GIT_ENABLED": "False",
   "POSH_INSTALLER": "manual",
-  "POSH_PID": "10484",
+  "POSH_PID": "4432",
   "POSH_SHELL_VERSION": "5.1.19041.5607",
   "POSH_THEME": "C:\\Users\\dell\\AppData\\Local\\oh-my-posh\\config.aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0phbkRlRG9iYmVsZWVyL29oLW15LXBvc2gvbWFpbi90aGVtZXMvamFuZGVkb2JiZWxlZXIub21wLmpzb24=.omp.json",
   "POSH_THEMES_PATH": "C:\\Users\\dell\\AppData\\Local\\Programs\\oh-my-posh\\themes",
@@ -85,6 +85,6 @@ window.env = {
   "windir": "C:\\WINDOWS",
   "WSLENV": "WT_SESSION:WT_PROFILE_ID:",
   "WT_PROFILE_ID": "{61c54bbd-c2c6-5271-96e7-009a87ff44bf}",
-  "WT_SESSION": "4c5805f5-5657-431e-b34d-1226aea70175",
+  "WT_SESSION": "f7990161-19af-4766-8631-76ff65db8ccb",
   "ZES_ENABLE_SYSMAN": "1"
 };

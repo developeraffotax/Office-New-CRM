fb6e61cba329202efd2838f278934157d309dab3
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-03-28 15:11:43 +0500

    activity sort add atasks/jobs  --deploy 3/28/25

--- client/public/env.js
+++ client/public/env.js
@@ -55,7 +55,7 @@ window.env = {
   "POSH_CURSOR_LINE": "7",
   "POSH_GIT_ENABLED": "False",
   "POSH_INSTALLER": "manual",
-  "POSH_PID": "5928",
+  "POSH_PID": "9352",
   "POSH_SHELL_VERSION": "5.1.19041.5607",
   "POSH_THEME": "C:\\Users\\dell\\AppData\\Local\\oh-my-posh\\config.aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0phbkRlRG9iYmVsZWVyL29oLW15LXBvc2gvbWFpbi90aGVtZXMvamFuZGVkb2JiZWxlZXIub21wLmpzb24=.omp.json",
   "POSH_THEMES_PATH": "C:\\Users\\dell\\AppData\\Local\\Programs\\oh-my-posh\\themes",
@@ -86,6 +86,6 @@ window.env = {
   "windir": "C:\\WINDOWS",
   "WSLENV": "WT_SESSION:WT_PROFILE_ID:",
   "WT_PROFILE_ID": "{61c54bbd-c2c6-5271-96e7-009a87ff44bf}",
-  "WT_SESSION": "5666d6a1-e25a-4475-9504-1fb239ac8723",
+  "WT_SESSION": "2724bbbd-a832-46c7-be04-b2f08af99deb",
   "ZES_ENABLE_SYSMAN": "1"
 };

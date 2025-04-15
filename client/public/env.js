6d105b8b4dea7e114d1c2b26ba11928dc4ed1ce7
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-04-15 19:21:49 +0500

    emailDetails.js myList.js update>>>

--- client/public/env.js
+++ client/public/env.js
@@ -55,7 +55,7 @@ window.env = {
   "POSH_CURSOR_LINE": "7",
   "POSH_GIT_ENABLED": "False",
   "POSH_INSTALLER": "manual",
-  "POSH_PID": "13256",
+  "POSH_PID": "3392",
   "POSH_SHELL_VERSION": "5.1.19041.5607",
   "POSH_THEME": "C:\\Users\\dell\\AppData\\Local\\oh-my-posh\\config.aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0phbkRlRG9iYmVsZWVyL29oLW15LXBvc2gvbWFpbi90aGVtZXMvamFuZGVkb2JiZWxlZXIub21wLmpzb24=.omp.json",
   "POSH_THEMES_PATH": "C:\\Users\\dell\\AppData\\Local\\Programs\\oh-my-posh\\themes",
@@ -86,6 +86,6 @@ window.env = {
   "windir": "C:\\WINDOWS",
   "WSLENV": "WT_SESSION:WT_PROFILE_ID:",
   "WT_PROFILE_ID": "{61c54bbd-c2c6-5271-96e7-009a87ff44bf}",
-  "WT_SESSION": "055141d1-2f23-4322-9d3c-678d38667ec7",
+  "WT_SESSION": "097ec3ad-bfd6-4b6f-a152-b1e7ff26e1bd",
   "ZES_ENABLE_SYSMAN": "1"
 };

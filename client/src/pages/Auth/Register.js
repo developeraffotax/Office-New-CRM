81d72ea94cc4ef8744c84db06804f8b38780059a
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-06-04 17:38:40 +0500

    select Juniors replaced with Select Team Memebers | v6042025

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -281,7 +281,7 @@ export default function Register({
 
         </div>
 
-        <h2 className="text-2xl font-semibold text-gray-800 mt-4 text-center">Select Juniors</h2>
+        <h2 className="text-2xl font-semibold text-gray-800 mt-4 text-center">Select Team Members</h2>
 
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3  bg-white rounded-xl  mt-2">
 

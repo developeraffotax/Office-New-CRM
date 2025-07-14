5e2102c9422f2e6650c5fac56a4245227417dcb0
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-07-14 20:15:29 +0500

    googsheet col order update

--- server/utils/googleSheet.js
+++ server/utils/googleSheet.js
@@ -25,17 +25,11 @@ const CREDENTIALS_PATH = path.join(
   "credentials.json"
 );
 
-
-
 // Create a JWT client using the Service Account key
 const jwtClient = new JWT({
   keyFile: CREDENTIALS_PATH,
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-   
 });
-
-
-
 
 // Authenticate with Google Sheets
 const authenticateGoogleSheets = async () => {
@@ -43,17 +37,11 @@ const authenticateGoogleSheets = async () => {
     throw new Error(`Credentials file not found at ${CREDENTIALS_PATH}`);
   }
 
-
-      
-
-
   const auth = new GoogleAuth({
     keyFile: CREDENTIALS_PATH,
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
-
- 
   return await auth.getClient();
 };
 
@@ -85,35 +73,35 @@ const flattenClientData = (client) => {
   } = client;
 
   return [
-    _id.toString(),
-    clientName,
-    regNumber,
     companyName,
-    email,
+    clientName,
+    job ? job.jobHolder : "",
+    job ? job.jobName : "",
     totalHours,
+    job && job.yearEnd ? new Date(job.yearEnd).toLocaleDateString() : "",
+    job && job.jobDeadline ? new Date(job.jobDeadline).toLocaleDateString() : "",
     currentDate ? new Date(currentDate).toLocaleDateString() : "",
+    job ? job.jobStatus : "",
+    job ? job.lead : "",
+    totalTime,
+    fee,
     source,
     clientType,
+    isActive ? "Yes" : "No",
+
+    regNumber,
+    email,
     country,
-    fee,
     ctLogin,
     pyeLogin,
     trLogin,
     vatLogin,
     authCode,
     utr,
-    isActive ? "Yes" : "No",
-    job ? job.jobName : "",
-    job && job.yearEnd ? new Date(job.yearEnd).toLocaleDateString() : "",
-    job && job.jobDeadline
-      ? new Date(job.jobDeadline).toLocaleDateString()
-      : "",
+
+    _id.toString(),
     job ? job.hours : "",
     job ? job.fee : "",
-    job ? job.jobStatus : "",
-    job ? job.lead : "",
-    job ? job.jobHolder : "",
-    totalTime,
     new Date(createdAt).toLocaleDateString(),
     new Date(updatedAt).toLocaleDateString(),
     label ? label.name : "",
@@ -124,54 +112,88 @@ const flattenClientData = (client) => {
 // Update Google Sheet with flattened data
 const updateGoogleSheet = async (data, range) => {
   try {
-
-
     //const authClient = await authenticateGoogleSheets();
 
-
-          await jwtClient.authorize();
+    await jwtClient.authorize();
 
     const sheets = google.sheets({ version: "v4", auth: jwtClient });
 
+    // const header = [
+    //   "ID",
+    //   "Client Name",
+    //   "Reg Number",
+    //   "Company Name",
+    //   "Email",
+    //   "Total Hours",
+    //   "Current Date",
+    //   "Source",
+    //   "Client Type",
+    //   "Country",
+    //   "Fee",
+    //   "CT Login",
+    //   "PYE Login",
+    //   "TR Login",
+    //   "VAT Login",
+    //   "Auth Code",
+    //   "UTR",
+    //   "Active",
+    //   "Job Name",
+    //   "Year End",
+    //   "Job Deadline",
+    //   "Hours",
+    //   "Job Fee",
+    //   "Job Status",
+    //   "Lead",
+    //   "Job Holder",
+    //   "Total Time",
+    //   "Created At",
+    //   "Updated At",
+    //   "Label Name",
+    //   "Label Color",
+    // ];
+
     const header = [
-      "ID",
-      "Client Name",
-      "Reg Number",
       "Company Name",
-      "Email",
+      "Client Name",
+      "Job Holder",
+      "Job Name",
       "Total Hours",
+      "Year End",
+      "Job Deadline",
       "Current Date",
+      "Job Status",
+      "Lead",
+      "Total Time",
+      "Fee",
       "Source",
       "Client Type",
+      "Active",
+
+      "Reg Number",
+      "Email",
       "Country",
-      "Fee",
       "CT Login",
       "PYE Login",
       "TR Login",
       "VAT Login",
       "Auth Code",
       "UTR",
-      "Active",
-      "Job Name",
-      "Year End",
-      "Job Deadline",
+
+      "ID",
       "Hours",
       "Job Fee",
-      "Job Status",
-      "Lead",
-      "Job Holder",
-      "Total Time",
       "Created At",
       "Updated At",
       "Label Name",
       "Label Color",
     ];
+
     const rows = data.map(flattenClientData);
 
     // Clear existing data on the specified sheet range
-   // const clearRange = range.split("!")[0] + "!A1:Z1000";
+    // const clearRange = range.split("!")[0] + "!A1:Z1000";
 
-      const clearRange = range.split("!")[0]; // e.g., "Client_jobs"
+    const clearRange = range.split("!")[0]; // e.g., "Client_jobs"
 
     await sheets.spreadsheets.values.clear({
       spreadsheetId: SHEET_ID,
@@ -229,3 +251,293 @@ export const sendDatatoGoogleSheet = async () => {
     console.error(error);
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { google } from "googleapis";
+// import { GoogleAuth, JWT } from "google-auth-library";
+// import jobsModel from "../models/jobsModel.js";
+// import dotenv from "dotenv";
+// import fs from "fs";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// dotenv.config();
+
+// const SHEET_ID = "1P5MY1-LD8pmEDwNIxILeLlKzcApKLKfRNcUtDoZnsnY";
+// const ACTIVE_SHEET_RANGE = "Client_jobs!A1";
+// const COMPLETED_SHEET_RANGE = "Completed!A1";
+// const INACTIVE_SHEET_RANGE = "Inactive_Clients!A1";
+
+// // Get __dirname
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Path for Credentials
+// const CREDENTIALS_PATH = path.join(
+//   __dirname,
+//   "..",
+//   "creds",
+//   "credentials.json"
+// );
+
+// // Create a JWT client using the Service Account key
+// const jwtClient = new JWT({
+//   keyFile: CREDENTIALS_PATH,
+//   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+
+// });
+
+// // Authenticate with Google Sheets
+// const authenticateGoogleSheets = async () => {
+//   if (!fs.existsSync(CREDENTIALS_PATH)) {
+//     throw new Error(`Credentials file not found at ${CREDENTIALS_PATH}`);
+//   }
+
+//   const auth = new GoogleAuth({
+//     keyFile: CREDENTIALS_PATH,
+//     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+//   });
+
+//   return await auth.getClient();
+// };
+
+// const flattenClientData = (client) => {
+//   const {
+//     _id,
+//     clientName,
+//     regNumber,
+//     companyName,
+//     email,
+//     totalHours,
+//     currentDate,
+//     source,
+//     clientType,
+//     country,
+//     fee,
+//     ctLogin,
+//     pyeLogin,
+//     trLogin,
+//     vatLogin,
+//     authCode,
+//     utr,
+//     isActive,
+//     job,
+//     totalTime,
+//     createdAt,
+//     updatedAt,
+//     label,
+//   } = client;
+
+//   return [
+//     _id.toString(),
+//     clientName,
+//     regNumber,
+//     companyName,
+//     email,
+//     totalHours,
+//     currentDate ? new Date(currentDate).toLocaleDateString() : "",
+//     source,
+//     clientType,
+//     country,
+//     fee,
+//     ctLogin,
+//     pyeLogin,
+//     trLogin,
+//     vatLogin,
+//     authCode,
+//     utr,
+//     isActive ? "Yes" : "No",
+//     job ? job.jobName : "",
+//     job && job.yearEnd ? new Date(job.yearEnd).toLocaleDateString() : "",
+//     job && job.jobDeadline
+//       ? new Date(job.jobDeadline).toLocaleDateString()
+//       : "",
+//     job ? job.hours : "",
+//     job ? job.fee : "",
+//     job ? job.jobStatus : "",
+//     job ? job.lead : "",
+//     job ? job.jobHolder : "",
+//     totalTime,
+//     new Date(createdAt).toLocaleDateString(),
+//     new Date(updatedAt).toLocaleDateString(),
+//     label ? label.name : "",
+//     label ? label.color : "",
+//   ];
+// };
+
+// // Update Google Sheet with flattened data
+// const updateGoogleSheet = async (data, range) => {
+//   try {
+
+//     //const authClient = await authenticateGoogleSheets();
+
+//           await jwtClient.authorize();
+
+//     const sheets = google.sheets({ version: "v4", auth: jwtClient });
+
+//     // const header = [
+//     //   "ID",
+//     //   "Client Name",
+//     //   "Reg Number",
+//     //   "Company Name",
+//     //   "Email",
+//     //   "Total Hours",
+//     //   "Current Date",
+//     //   "Source",
+//     //   "Client Type",
+//     //   "Country",
+//     //   "Fee",
+//     //   "CT Login",
+//     //   "PYE Login",
+//     //   "TR Login",
+//     //   "VAT Login",
+//     //   "Auth Code",
+//     //   "UTR",
+//     //   "Active",
+//     //   "Job Name",
+//     //   "Year End",
+//     //   "Job Deadline",
+//     //   "Hours",
+//     //   "Job Fee",
+//     //   "Job Status",
+//     //   "Lead",
+//     //   "Job Holder",
+//     //   "Total Time",
+//     //   "Created At",
+//     //   "Updated At",
+//     //   "Label Name",
+//     //   "Label Color",
+//     // ];
+
+//     const header = [
+//       "ID",
+//       "Client Name",
+//       "Reg Number",
+//       "Company Name",
+//       "Email",
+//       "Total Hours",
+//       "Current Date",
+//       "Source",
+//       "Client Type",
+//       "Country",
+//       "Fee",
+//       "CT Login",
+//       "PYE Login",
+//       "TR Login",
+//       "VAT Login",
+//       "Auth Code",
+//       "UTR",
+//       "Active",
+//       "Job Name",
+//       "Year End",
+//       "Job Deadline",
+//       "Hours",
+//       "Job Fee",
+//       "Job Status",
+//       "Lead",
+//       "Job Holder",
+//       "Total Time",
+//       "Created At",
+//       "Updated At",
+//       "Label Name",
+//       "Label Color",
+//     ];
+
+//     const rows = data.map(flattenClientData);
+
+//     // Clear existing data on the specified sheet range
+//    // const clearRange = range.split("!")[0] + "!A1:Z1000";
+
+//       const clearRange = range.split("!")[0]; // e.g., "Client_jobs"
+
+//     await sheets.spreadsheets.values.clear({
+//       spreadsheetId: SHEET_ID,
+//       range: clearRange,
+//     });
+
+//     console.log("range:", range);
+
+//     // Append new data to the specified sheet range
+//     await sheets.spreadsheets.values.update({
+//       spreadsheetId: SHEET_ID,
+//       range: range,
+//       valueInputOption: "RAW",
+//       requestBody: {
+//         values: [header, ...rows],
+//       },
+//     });
+//     console.log(`Google Sheet updated successfully at ${range}`);
+//   } catch (error) {
+//     console.error("Error updating Google Sheet:", error);
+//     throw error;
+//   }
+// };
+
+// // Send data to Google Sheets
+// export const sendDatatoGoogleSheet = async () => {
+//   try {
+//     // Fetch active clients
+//     const activeClients = await jobsModel
+//       .find({
+//         status: { $ne: "completed" },
+//         "job.jobStatus": { $ne: "Inactive" },
+//       })
+//       .populate("data");
+
+//     // Fetch completed clients
+//     const completedClients = await jobsModel
+//       .find({ status: "completed" })
+//       .populate("data");
+
+//     // Inactive Clients
+//     const inactiveClients = await jobsModel
+//       .find({ "job.jobStatus": "Inactive" })
+//       .populate("data");
+
+//     // Update both sheets
+//     await updateGoogleSheet(activeClients, ACTIVE_SHEET_RANGE);
+//     await updateGoogleSheet(completedClients, COMPLETED_SHEET_RANGE);
+//     await updateGoogleSheet(inactiveClients, INACTIVE_SHEET_RANGE);
+
+//     console.log(
+//       "Google Sheets updated successfully with active, inactive and completed clients!"
+//     );
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };

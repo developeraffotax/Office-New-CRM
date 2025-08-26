603075a481da918c8c138216388237542854afc5
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-08-26 12:37:10 +0500

    task deadline fitle update

--- client/src/pages/Tasks/AllTasks.js
+++ client/src/pages/Tasks/AllTasks.js
@@ -2422,6 +2422,12 @@ Cell: ({ cell, row }) => {
             today.getMonth(),
             today.getDate()
           );
+
+          
+          const tomorrow = new Date(today);
+          tomorrow.setDate(today.getDate() + 1);
+
+          
           switch (filterValue) {
             case "Expired":
               return cellDate < startOfToday;
@@ -2434,29 +2440,32 @@ Cell: ({ cell, row }) => {
             case "Today":
               return cellDate.toDateString() === today.toDateString();
             case "Tomorrow":
-              const tomorrow = new Date(today);
-              tomorrow.setDate(today.getDate() + 1);
+               
               return cellDate.toDateString() === tomorrow.toDateString();
             case "In 7 days":
               const in7Days = new Date(today);
               in7Days.setDate(today.getDate() + 7);
-              return cellDate <= in7Days && cellDate > today;
+
+              
+
+
+              return cellDate <= in7Days && cellDate > tomorrow;
             case "In 15 days":
               const in15Days = new Date(today);
               in15Days.setDate(today.getDate() + 15);
-              return cellDate <= in15Days && cellDate > today;
+              return cellDate <= in15Days && cellDate > tomorrow;
             case "30 Days":
               const in30Days = new Date(today);
               in30Days.setDate(today.getDate() + 30);
-              return cellDate <= in30Days && cellDate > today;
+              return cellDate <= in30Days && cellDate > tomorrow;
             case "60 Days":
               const in60Days = new Date(today);
               in60Days.setDate(today.getDate() + 60);
-              return cellDate <= in60Days && cellDate > today;
+              return cellDate <= in60Days && cellDate > tomorrow;
             case "Last 12 months":
               const lastYear = new Date(today);
               lastYear.setFullYear(today.getFullYear() - 1);
-              return cellDate >= lastYear && cellDate <= today;
+              return cellDate >= lastYear && cellDate <= tomorrow;
             default:
               return false;
           }

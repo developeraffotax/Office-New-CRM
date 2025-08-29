92d2379c6f9140c977a71d9336e27776814af3cf
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-08-29 15:21:49 +0500

    getAllJobs optimized mdb query

--- client/public/env.js
+++ client/public/env.js
@@ -57,7 +57,7 @@ window.env = {
   "POSH_CURSOR_LINE": "7",
   "POSH_GIT_ENABLED": "False",
   "POSH_INSTALLER": "manual",
-  "POSH_PID": "13708",
+  "POSH_PID": "1764",
   "POSH_SHELL_VERSION": "5.1.19041.5607",
   "POSH_THEME": "C:\\Users\\dell\\AppData\\Local\\oh-my-posh\\config.aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0phbkRlRG9iYmVsZWVyL29oLW15LXBvc2gvbWFpbi90aGVtZXMvamFuZGVkb2JiZWxlZXIub21wLmpzb24=.omp.json",
   "POSH_THEMES_PATH": "C:\\Users\\dell\\AppData\\Local\\Programs\\oh-my-posh\\themes",
@@ -88,6 +88,6 @@ window.env = {
   "windir": "C:\\WINDOWS",
   "WSLENV": "WT_SESSION:WT_PROFILE_ID:",
   "WT_PROFILE_ID": "{61c54bbd-c2c6-5271-96e7-009a87ff44bf}",
-  "WT_SESSION": "35dfb4e9-6cb8-451c-a551-2650b202769a",
+  "WT_SESSION": "d52ebc15-6e64-403a-96f3-4033ee06383d",
   "ZES_ENABLE_SYSMAN": "1"
 };
--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -207,13 +207,21 @@ export const getAllClients = async (req, res) => {
 
     const clients = await jobsModel
       .find({
-        status: { $ne: "completed" },
+          status: { $eq: "process" },
+
+        //  $or: [
+        //     {status: { $in: ["process", ""] }},    // status = ""
+        //     { status: { $exists: false } } // status field does not exist
+        //   ],
+
+          // add eq
         "job.jobStatus": { $ne: "Inactive" },
+        // "job.jobStatus": { $in: ["Quote", "Data", "Progress", "Queries", "Approval", "Submission", "Billing", "Feedback", "", "empty"] },
       })
       .select(
         "clientName companyName regNumber email fee currentDate totalHours totalTime job.jobName job.yearEnd job.jobDeadline job.workDeadline job.jobStatus job.lead job.jobHolder comments._id comments.status label source data activeClient clientType partner clientPaidFee"
       )
-      .populate("data");
+      .populate("data").lean();
 
 
 

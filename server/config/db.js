b7e206c0bab6ed489cf509329e6c9d3bb23be02d
ahtshamsaleem <ahtshamsaleem51@gmail.com>
2025-09-12 13:14:32 +0500

    created date filter update in leads.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,13 @@ import color from "colors";
 
 export const connectDB = async (req, res) => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
+
+
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 30000, // 30s timeout
+  family: 4, // force IPv4 (Atlas sometimes has IPv6 issues)
+});
     console.log(
       `Successfully connected to MongoDB ${conn.connection.host}`.bgGreen.white
     );
